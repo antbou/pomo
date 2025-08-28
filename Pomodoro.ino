e51cf41e1bec7b4cee147e8554d1fbd495e08857
antho <bouillant.anthony@gmail.com>
2025-08-28 18:21:09 +0200

    Initial commit

---
+++ Pomodoro.ino
@@ -0,0 +1,56 @@
+/// @file    Pomodoro.ino
+/// @brief   Simple Pomodoro timer with WS2812 LED strip
+
+#include <Arduino.h>
+#include <FastLED.h>
+
+// -------------------- Configuration --------------------
+#define DATA_PIN       5
+#define NUM_LEDS       25
+#define SESSION_COUNT  4   // number of work+break cycles
+#define WORK_MINUTES   25
+#define BREAK_MINUTES  5
+#define BRIGHTNESS     64  // 0..255
+
+CRGB leds[NUM_LEDS];
+
+
+void setup() {
+  FastLED.addLeds<WS2812, DATA_PIN, GRB>(leds, NUM_LEDS);
+  FastLED.setBrightness(BRIGHTNESS);
+  FastLED.clear(true);
+  Serial.begin(9600);
+}
+
+void loop() {
+  for (int i = 0; i < SESSION_COUNT; i++) {
+    run_work_session(WORK_MINUTES);
+    run_break_session(BREAK_MINUTES);
+  }
+
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(500);
+  FastLED.clear(true);
+}
+
+
+long minutes_to_ms(int minutes) {
+  return minutes * 60000;
+}
+
+void run_work_session(int minutes) {
+  long step_delay = minutes_to_ms(minutes) / NUM_LEDS;
+  for (int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Blue;
+    FastLED.show();
+    delay(step_delay);
+  }
+}
+
+void run_break_session(int minutes) {
+  fill_solid(leds, NUM_LEDS, CRGB::Green);
+  FastLED.show();
+  delay(minutes_to_ms(minutes));
+  FastLED.clear(true);
+}
